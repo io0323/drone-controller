f3b8f072c46a65a123ce4549fedd201063b90358
io0323 <iokuma0323@gmail.com>
2026-04-12 20:16:23 +0900

    feat: BLEスキャン前にプラットフォーム別の権限要求を追加

---
+++ composeApp/src/iosMain/kotlin/com/io/dronecontroller/ui/ble/BlePermission.ios.kt
@@ -0,0 +1,6 @@
+package com.io.dronecontroller.ui.ble
+
+import androidx.compose.runtime.Composable
+
+@Composable
+actual fun rememberBlePermissionLauncher(onGranted: () -> Unit): () -> Unit = onGranted
